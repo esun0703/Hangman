5f1a9fcaa6467cf668695f1a7751b5df1218d144
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2016-11-30 18:15:47 -0500

    completed guess counter and functions

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,40 +1,132 @@
+//Variables
+//===================================================================
 //Beginning of letters//
 var wordbank = [
-	'Zeppelin',
-	'Sabbath',
-	'Beatles', 
-	'Hendrix',
-	'Drummer',
-	'KISS',
-	"Guitar",
+	'zeppelin',
+	'sabbath',
+	'beatles', 
+	'hendrix',
+	'drummer',
+	'kiss',
+	"guitar",
 ]
 
-
-//Word choosing
-var randomWordGenerator = Math.floor(Math.random() * wordbank.length);
-var chosenWord=wordbank[randomWordGenerator];
-var chosenWordArr=chosenWord.split("");
-var displayArr=[];
+var chosenWord = wordbank[Math.floor(Math.random() * wordbank.length)];
+var lettersInWord=chosenWord.split("");
+var numBlanks=lettersInWord.length;
+var blanksAndWins=[];//z _ p p _ i _
+var wrongLetters = [];
 
 //counters
-var maxCount=20;
-var count=0;
-var guessesLeft = (maxCount - count);
-
-//Tells You the Word In The Console Log
-console.log("Spoiler: The Word Is: ", chosenWord);
+var winCount=0;
+var lossCount=0;
+var guessesLeft = 10;
 
 
-//choses random words and allows 
-	for(var i=0; i < chosenWord.length; i++) {
-		 displayArr.push(chosenWordArr[i].replace(chosenWordArr[i],"_"));
-		 
+//Functions
+//=================================================================
+function startGame (){
+
+	//Reset
+	guessesLeft = 10;
+	wrongLetters =[];
+	blanksAndWins=[];
+	
+
+	chosenWord = wordbank[Math.floor(Math.random() * wordbank.length)];
+	lettersInWord=chosenWord.split("");
+	numBlanks=lettersInWord.length;
+
+	//Populate rightOrWrong with right number of blanks
+	for(var i=0; i < numBlanks; i++) {
+		 blanksAndWins.push("_");
 	} 
 
-	 document.querySelector("#wordDisplay").innerHTML=displayArr.join(" ");
+	//change HTML
+	 document.querySelector("#wordDisplay").innerHTML=blanksAndWins.join(" ");
+	 document.querySelector("#guessesDisplay").innerHTML=guessesLeft;
 
 
+	//Testing/Debugging
+	console.log(chosenWord);
+	console.log(lettersInWord);
+	console.log(numBlanks + " letters in this word");
+	console.log(blanksAndWins);
 
+}
+
+function checkLetters(letter){
+	//checks if letter is in the word
+	var isLetterInWord = false;
+	
+	for (var i=0; i<numBlanks;i++){
+		if(chosenWord[i]==letter){
+			isLetterInWord=true;
+		}
+	}
+
+	//Check where in the word the letter exists then populate out blanks and Successes array.
+	if(isLetterInWord){
+		for (var i=0; i<numBlanks;i++){
+			if(chosenWord[i]==letter){
+				blanksAndWins[i]=letter;
+			}
+		}
+	}
+
+	//Letter Wasn't Found
+	else{
+		wrongLetters.push(letter);
+		guessesLeft--;
+	}
+	//Testing and Debugging
+	console.log(blanksAndWins)
+
+}
+
+function roundComplete(){
+	console.log("Win Count: " + winCount + "| Loss Count: " + lossCount + "| Guesses Left: "+ guessesLeft);
+	
+	//Update The HTML TO Reflect//
+	document.querySelector("#guessesDisplay").innerHTML=guessesLeft;
+	document.querySelector("#wordDisplay").innerHTML=blanksAndWins.join(" ");
+	
+	//Check If User Won
+	if(lettersInWord.toString()==blanksAndWins.toString()){
+		winCount++
+		alert("You Won!");
+
+		startGame();
+	}
+
+	else if(guessesLeft==0){
+		lossCount++;
+		alert("You Lost!");
+
+		startGame();
+	}
+
+	
+
+}
+	
+
+//Main Process
+//==================================================================
+//Initiates the code the first time
+startGame();
+
+//Registers keyclikcs
+
+document.onkeyup=function(event){
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	checkLetters(letterGuessed)
+	roundComplete();
+	
+
+	//Testing/Debugging
+	console.log(letterGuessed);
+}
 
 
  
